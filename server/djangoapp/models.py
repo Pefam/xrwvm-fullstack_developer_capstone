06be2c681d9bb4967d78e59922da563ab6e749ec
Pefam <159665558+Pefam@users.noreply.github.com>
2025-07-27 19:40:22 -0600

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,4 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
-from django.utils.timezone import now
-from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -15,9 +11,10 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    
+
     def __str__(self):
         return self.name
+
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
@@ -37,12 +34,12 @@ class CarModel(models.Model):
         (SUV, 'SUV'),
         (WAGON, 'Wagon'),
     ]
-    
+
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     dealer_id = models.IntegerField()
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=10, choices=CAR_TYPES)
     year = models.DateField()
-    
+
     def __str__(self):
         return f"{self.car_make.name} {self.name}"
