e14cd1f04d1fbdb3d89908b394263ff9d882b1f0
Pefam <159665558+Pefam@users.noreply.github.com>
2025-07-27 20:10:56 -0600

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,15 +1,9 @@
-# Uncomment the required imports before adding the code
-
-from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, redirect
-from django.contrib.auth import logout
-from django.contrib import messages
+from django.contrib.auth import logout, login, authenticate
 from django.http import JsonResponse
-from django.contrib.auth import login, authenticate
+from django.views.decorators.csrf import csrf_exempt
 import logging
 import json
-from django.views.decorators.csrf import csrf_exempt
 
 from .populate import initiate
 from .models import CarMake, CarModel
